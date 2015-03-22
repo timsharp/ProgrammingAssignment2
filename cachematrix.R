53b01a97a369b435afa03d30e7e5062dfe09c924
timsharp <tsharp@columbus.rr.com>
2015-03-21 21:54:28 -0400

    Assignment #2
    This is my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2 Lexical Scoping
+## FUNCTION: makeCacheMatrix
+## DESCRIPTION:creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(v_matrix = matrix()) { 
+  v_inverse <- NULL 
+  set       <- function(x) { 
+    v_matrix     <<- x    ;                 
+    v_inverse    <<- NULL ; 
+  } 
+  
+  get    <- function()    return(v_matrix)         ; 
+  setinv <- function(inv) v_inverse <<- inv        ; 
+  getinv <- function()    return(v_inverse)        ; 
+  
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv)) 
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+## Programming Assignment #2 Lexical Scoping
+## FUNCTION: cacheSolve
+## DESCRIPTION:This function computes the inverse of the special
+##            "matrix" returned by makeCacheMatrix.
+##             If the inverse has already been calculated 
+##            (and the matrix has not changed), then the cachesolve
+##             should retrieve the inverse from the cache.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(v_matrix, ...) { 
+  v_inverse <- v_matrix$getinv() 
+  if(!is.null(v_inverse)) { 
+    message("get the data in the cache")  ## Return a matrix that is the inverse of 'x'
+    return(v_inverse) 
+  } 
+  data <- v_matrix$get() 
+  v_inverse <- solve(data, ...) 
+  v_matrix$setinv(v_inverse) 
+  return(v_inverse) 
+} 
